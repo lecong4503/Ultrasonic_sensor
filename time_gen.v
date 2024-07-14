f24fb713da672936bf04dd2a4a27cc25af9c0919
Lecong4503 <Lecong4503@gmail.com>
2024-07-14 23:15:29 +0900

    feat : ms_cnt 기능 추가

--- time_gen.v
+++ time_gen.v
@@ -9,6 +9,7 @@ module time_gen (
     output [5:0] ms_cnt
 );
 
+reg [13:0] r_us_cnt;
 reg [6:0] clk_cnt;
 reg r_us_tick;
 reg r_ms_tick;
@@ -25,6 +26,24 @@ always @ (posedge clk or negedge rst_n) begin
             clk_cnt <= cnt_clk + 1;
             r_us_tick <= 0;
         end
+    end
+end
+
+always @ (posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        r_us_cnt <= 0;
+        r_ms_tick <= 0;
+    end else if (i_run) begin
+        if (r_us_cnt == 999) begin
+            r_us_cnt <= 0;
+            r_ms_tick <= 1;
+        end else begin
+            r_us_cnt <= r_us_cnt + 1;
+            r_ms_tick <= 0;
+        end
+    end else if (i_idle) begin
+        r_us_cnt <= 0;
+        r_ms_tick <= 0;
     end
 end
 
